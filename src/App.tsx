6711e8bd90ce06142474a31fb8e030ee834098f7
yasuaki-watanabe <yasuaki-watanabe@dip-net.co.jp>
2022-07-11 18:26:23 +0900

    destroy

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
-import { Todo } from './Todo';
-import { TodoType } from './types/todo';
-import { Text } from './Text';
-import { UserProfile } from './UserProfile';
-import { User } from './types/user';
-
-const user: User = {
-  name: 'jakeee',
-  hobbies: ['watching movie', 'playing video game'],
-};
 
 function App() {
-  const [todos, setTodos] = useState<TodoType[]>([]);
-
-  const onClickFetchData = () => {
-    axios
-      .get<TodoType[]>('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data));
-  };
-
   return (
     <div className="App">
-      <UserProfile user={user} />
-      <Text color="red" fontSize="18px" />
-      <button onClick={onClickFetchData}>Fetch data</button>
-      {todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          title={todo.title}
-          userId={todo.userId}
-          completed={todo.completed}
-        />
-      ))}
+      <h1>destroyed</h1>
     </div>
   );
 }
--- src/Todo.tsx
+++
@@ -1,8 +0,0 @@
-import { TodoType } from './types/todo';
-import { FC } from 'react';
-
-export const Todo: FC<Omit<TodoType, 'id'>> = (props) => {
-  const { title, userId, completed = false } = props;
-  const completeMark = completed ? '[Done]' : '[Yet]';
-  return <p>{`${completeMark} ${title} (user: ${userId})`}</p>;
-};
