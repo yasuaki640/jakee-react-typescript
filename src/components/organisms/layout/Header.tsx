4ddf24a06a8875db3b2117249a945666a1b97396
yasuaki640 <yasuaki640@gmail.com>
2022-07-19 13:49:18 +0900

    header

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,68 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
-export const Header: FC = () => {
-  return <div style={{ height: '50px', backgroundColor: 'red' }}></div>;
-};
+export const Header: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer' }}>
+          <Heading as="h1" fontStyle={{ base: 'md', md: 'lg' }}>
+            Awesome User Management App
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: 'none', md: 'flex' }}
+        >
+          <Box pr={4}>
+            <Link>User List</Link>
+          </Box>
+          <Link>Setting</Link>
+        </Flex>
+        <IconButton
+          aria-label="Menu Button"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          display={{ base: 'block', md: 'none' }}
+          onClick={onOpen}
+        />
+      </Flex>
+      <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">User List</Button>
+              <Button w="100%">Setting</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  );
+});
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from '../components/pages/Login';
 import { homeRoutes } from './HomeRoutes';
 import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
 
 export const Router: FC = memo(() => {
   return (
@@ -10,7 +11,11 @@ export const Router: FC = memo(() => {
       <Route path="/" element={<Login />} />
       <Route path="/home">
         {homeRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.children} />
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<HeaderLayout children={route.children} />}
+          />
         ))}
       </Route>
       <Route path="*" element={<Page404 />} />
