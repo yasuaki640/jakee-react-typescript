d7e6cc2dac72c2301f9165710e5d8e46639547d9
yasuaki640 <yasuaki640@gmail.com>
2022-07-19 11:13:01 +0900

    routing

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -2,17 +2,22 @@ import { Home } from '../pages/Home';
 import { UserManagement } from '../pages/UserManagement';
 import { Setting } from '../pages/Setting';
 
-export const homeRoutes = [
+type Route = {
+  path: string;
+  children: JSX.Element;
+};
+
+export const homeRoutes: Route[] = [
   {
-    path: '/',
+    path: '',
     children: <Home />,
   },
   {
-    path: '/user_management',
+    path: 'user_management',
     children: <UserManagement />,
   },
   {
-    path: '/setting',
+    path: 'setting',
     children: <Setting />,
   },
 ];
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,17 @@
 import { FC, memo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Login } from '../pages/Login';
+import { homeRoutes } from './HomeRoutes';
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" />
+      <Route path="/home">
+        {homeRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.children} />
+        ))}
+      </Route>
     </Routes>
   );
 });
